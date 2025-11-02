e4ab706084fe64154bc42b7045e98cfdb4f337b2
melanierefman <87106838+melanierefman@users.noreply.github.com>
2025-11-03 02:31:30 +0700

    fix minor error

--- src/pages/user/certificate/index.jsx
+++ src/pages/user/certificate/index.jsx
@@ -8,7 +8,7 @@ import {
   StyleSheet,
   Image as PdfImage,
 } from "@react-pdf/renderer";
-import Loading from "../../../components/Loading";
+import Loading from "../../../components/loading";
 import bgImage from "../../../assets/cert.jpg";
 
 const styles = StyleSheet.create({
