aa14b15f5246a28b6a3c07c881968a80836398ff
melanierefman <87106838+melanierefman@users.noreply.github.com>
2025-11-12 16:10:00 +0700

    fix: error

--- src/pages/admin/event-detail/index.jsx
+++ src/pages/admin/event-detail/index.jsx
@@ -1163,7 +1163,7 @@ const AdminDetail = () => {
         <FinishConfirmModal
           isOpen={showFinishConfirm}
           onClose={() => setShowFinishConfirm(false)}
-          onConfirm={handleFinishEvent} // kirim langsung handler lama
+          onConfirm={handleFinishEvent}
           isLoading={isFinishing}
         />
 
