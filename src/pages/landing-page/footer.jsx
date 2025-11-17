3908cd49162b8cfdd969e8744bf0e521d0f16915
zainsuroya <nunik.suroya@gmail.com>
2025-11-17 17:14:03 +0700

    add teks Prototipe dan uji coba

--- src/pages/landing-page/footer.jsx
+++ src/pages/landing-page/footer.jsx
@@ -4,9 +4,17 @@ export default function Footer() {
   return (
     <footer className="bg-white py-8 text-center text-sm text-gray-500">
       <Typography type="caption2" className="px-4 text-center md:px-0">
-        Sistem untuk mengelola berbagai acara AirNav Indonesia secara efisien dan profesional.
+        Sistem untuk mengelola berbagai acara AirNav Indonesia secara efisien
+        dan profesional.
       </Typography>
-      <div className="mt-3">© 2025 AirNav Indonesia. All rights reserved.</div>
+
+      <Typography
+        type="caption2"
+        className="px-4 text-center md:px-0 font-semibold italic text-blue-600 dark:text-blue-400"
+      >
+        Prototipe dan uji coba
+      </Typography>
+      <div className="">© 2025 AirNav Indonesia. All rights reserved.</div>
     </footer>
   );
 }
